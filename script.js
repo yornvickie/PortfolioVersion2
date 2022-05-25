f34dc4f0fc082718b47c913c34e9facc2a48b289
yornvickie <yornvickie@gmail.com>
2022-05-24 19:18:08 -0500

    added New Portfolio Project

---
+++ script.js
@@ -0,0 +1,3 @@
+// let absoluteHeight = document.getElementsByClassName('absolute')[0].offsetHeight;
+// let blankDiv = document.getElementsByClassName('blankDiv')[0];
+// blankDiv.style.height = absoluteHeight + 5 + "px";
